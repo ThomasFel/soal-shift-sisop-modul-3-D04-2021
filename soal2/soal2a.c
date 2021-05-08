1a6fa03d58b17046067d7be9da14e2b453799ef3
ThomasFelix <37539546+ThomasFel@users.noreply.github.com>
2021-05-08 19:46:51 +0700

    Kurang 2b :(

---
+++ soal2/soal2a.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int matriksX[4][3] = {
+    {1, 1, 1},
+    {1, 2, 3},
+    {1, 1, 1},
+    {2, 2, 2}
+};
+
+int matriksY[3][6] = {
+    {1, 2, 3, 4, 5, 6},
+    {6, 5, 4, 3, 2, 1},
+    {1, 2, 4, 4, 1, 2}
+};
+
+int matriksZ[4][6];
+
+struct args {
+    int i;
+    int j;
+};
+
+void *perkalian(void *arg) {
+    int i = ((struct args*) arg) -> i;
+    int j = ((struct args*) arg) -> j;
+
+    for (int k = 0; k < 3; k++) {
+        matriksZ[i][j] += matriksX[i][k] * matriksY[k][j];
+    }
+}
+
+int main() {
+    pthread_t tid[4][6];
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 6; j++) {
+            struct args *index = (struct args*) malloc(sizeof(struct args));
+            index -> i = i;
+            index -> j = j;
+            pthread_create(&tid[i][j], NULL, &perkalian, (void *)index);
+        }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 6; j++) {
+            pthread_join(tid[i][j], NULL);
+        }
+    }
+
+    printf("Matriks: \n");
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 6; j++) {
+            printf("%4d", matriksZ[i][j]);
+        }
+        printf("\n");
+    }
+    
+    key_t key = 1234;
+    int *value;
+
+    int shmid = shmget(key, sizeof(matriksZ), IPC_CREAT | 0666);
+    value = shmat(shmid, NULL, 0);
+
+    int *p = (int *) value;
+
+    memcpy(p, matriksZ, 95);
+
+    shmdt(value);
+    
+    return 0;
+}
---
+++ soal2/soal2c.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+int main(int argc, char **argv) {
+    int fd[4];
+    int status;
+    char *ps_aux_arg[] = {"ps", "aux", NULL};
+    char *sort_arg[] = {"sort", "-nrk", "3,3", NULL};
+    char *head_arg[] = {"head", "-5", NULL};
+
+    pipe(fd);
+    pipe(fd + 2);
+
+    if (fork() == 0) {
+        dup2(fd[1], STDOUT_FILENO);
+
+        close(fd[0]);
+        close(fd[1]);
+        close(fd[2]);
+        close(fd[3]);
+
+        execvp(*ps_aux_arg, ps_aux_arg);
+    }
+
+    else {
+        if (fork() == 0) {
+            dup2(fd[0], STDIN_FILENO);
+            dup2(fd[3], STDOUT_FILENO);
+
+            close(fd[0]);
+            close(fd[1]);
+            close(fd[2]);
+            close(fd[3]);
+
+            execvp(*sort_arg, sort_arg);
+        }
+
+        else {
+            if (fork() == 0) {
+                dup2(fd[2], STDIN_FILENO);
+
+                close(fd[0]);
+                close(fd[1]);
+                close(fd[2]);
+                close(fd[3]);
+
+                execvp(*head_arg, head_arg);
+            }
+        }
+    }
+
+    close(fd[0]);
+    close(fd[1]);
+    close(fd[2]);
+    close(fd[3]);
+
+    for (int i = 1; i < 3; i++) {
+        wait(&status);
+    }
+
+    return 0;
+}
